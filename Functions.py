e05bb6c1efbe297a31ce907fb695920bf3c0df37
Nour-Cheour10 <nour.cheour@insat.ucar.tn>
2024-04-03 12:25:04 +0200

    added token input

--- Functions.py
+++ Functions.py
@@ -22,14 +22,10 @@ from bqplot import (
     DateScale
 )
 
-config = configparser.ConfigParser()
-config.read('.config')
-token = config['DEFAULT']['token']
-
 #Carbon Intensity per day function
 async def Carbon_Intensity_per_day(x,bar,token):
         async with ElectricityMaps(token=token) as em:
-                response = await em.latest_carbon_intensity(ZoneRequest(x))
+                response = await em.latest_carbon_intensity(ZoneRequest(x),token)
         param1 = response.carbon_intensity
         param2 = response.timestamp
         consumption_data = param1
@@ -39,55 +35,73 @@ async def Carbon_Intensity_per_day(x,bar,token):
     
 #Hourly Power Consumption per day function
 async def Hourly_Power_Consumption(country, bar, token):
-    x= await Countries_Abrreviation_N(country)
-    async with ElectricityMaps(token=token) as em:
-        response_history = await em.power_breakdown_history(ZoneRequest(x))
-    param1 = [param.time for param in response_history.history]
-    param2 = [param.power_consumption_total for param in response_history.history]
-    #datetime_data = np.array(param2, dtype="datetime64[s]")
-    datetime_data = np.array([param1], dtype="datetime64")
-    Power_Consumption = param2
-    bar.x = datetime_data
-    bar.y = Power_Consumption
+    try:
+        x= await Countries_Abrreviation_N(country,token)
+        async with ElectricityMaps(token=token) as em:
+            response_history = await em.power_breakdown_history(ZoneRequest(x))
+        param1 = [param.time for param in response_history.history]
+        param2 = [param.power_consumption_total for param in response_history.history]
+        #datetime_data = np.array(param2, dtype="datetime64[s]")
+        datetime_data = np.array([param1], dtype="datetime64")
+        Power_Consumption = param2
+        bar.x = datetime_data
+        bar.y = Power_Consumption
+    except:
+        bar.x=[]
+        bar.y=[]
 
 
 #Hourly Import Export Power Consumption
 async def Hourly_Imp_Exp_Power_Consumption(country, Exp_bar,Imp_bar, token):
-    x= await Countries_Abrreviation_N(country)
-    async with ElectricityMaps(token=token) as em:
-        response_history = await em.power_breakdown_history(ZoneRequest(x))
-    param1 = [param.power_export_total for param in response_history.history]
-    param2 = [param.power_import_total for param in response_history.history]
-    datetime_data = np.array(param1, dtype="datetime64[s]")
-    Power_Consumption = param2
-    Exp_bar.x = datetime_data
-    Imp_bar.x = datetime_data
-    Exp_bar.y = param1
-    Imp_bar.y = param2
-
+    try:
+        x= await Countries_Abrreviation_N(country,token)
+        async with ElectricityMaps(token=token) as em:
+            response_history = await em.power_breakdown_history(ZoneRequest(x))
+        param1 = [param.power_export_total for param in response_history.history]
+        param2 = [param.power_import_total for param in response_history.history]
+        datetime_data = np.array(param1, dtype="datetime64[s]")
+        Power_Consumption = param2
+        Exp_bar.x = datetime_data
+        Imp_bar.x = datetime_data
+        Exp_bar.y = param1
+        Imp_bar.y = param2
+    except:
+        Exp_bar.x = []
+        Imp_bar.x = []
+        Exp_bar.y = []
+        Imp_bar.y = []
+        
 #Power consumption by source
 async def Power_Consumption_Source(country,bar,token):
-    x= await Countries_Abrreviation_N(country)
-    async with ElectricityMaps(token=token) as em:
-            response_history= await em.power_breakdown_history(ZoneRequest(x))
-    data=response_history.history[0].power_consumption_breakdown
-    categories = list(data.keys())
-    values = list(data.values()) 
-    bar.x =categories
-    bar.y=values
+    try:
+        x= await Countries_Abrreviation_N(country,token)
+        async with ElectricityMaps(token=token) as em:
+                response_history= await em.power_breakdown_history(ZoneRequest(x))
+        data=response_history.history[0].power_consumption_breakdown
+        categories = list(data.keys())
+        values = list(data.values()) 
+        bar.x =categories
+        bar.y=values
+    except:
+        bar.x=[]
+        bar.y=[]
 
 #Power imported by country
 async def Power_Imported_Country(country,bar,token):
-    x= await Countries_Abrreviation_N(country)
-    async with ElectricityMaps(token=token) as em:
-        response_history= await em.power_breakdown_history(ZoneRequest(x))
-    data=response_history.history[0].power_import_breakdown
-    bar.x=list(data.keys())
-    bar.y=list(data.values())
+    try:
+        x= await Countries_Abrreviation_N(country,token)
+        async with ElectricityMaps(token=token) as em:
+            response_history= await em.power_breakdown_history(ZoneRequest(x))
+        data=response_history.history[0].power_import_breakdown
+        bar.x=list(data.keys())
+        bar.y=list(data.values())
+    except:
+        bar.x=[]
+        bar.y=[]
 
 
 #Trouver abrreviation du pays
-async def Countries_Abrreviation_N(country):
+async def Countries_Abrreviation_N(country,token):
     async with ElectricityMaps(token=token) as em:
             response_history= await em.zones()
     countries = {key: zone.zone_name for key, zone in response_history.items()}
